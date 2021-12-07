181e047b805f7705a47c63a8abd89eee9f4d6541
wangyaxian <wangyaxiandev@gmail.com>
2021-12-08 07:48:12 +0800

    init

--- app/Module/User/Action/OAuthAction.php
+++ app/Module/User/Action/OAuthAction.php
@@ -8,6 +8,7 @@ use App\Util\HttpUtil;
 use App\Util\Log;
 use App\Util\Redis;
 use App\Util\Util;
+use GuzzleHttp\Exception\GuzzleException;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
@@ -80,6 +81,7 @@ class OAuthAction
             return HttpUtil::error($response);
         }
 
+        // 1、根据 GitHub 回调信息中的 code 信息去获取 GitHub 的 access_token
         $getAccessTokenUrl          = 'https://github.com/login/oauth/access_token';
         $clientId                   = env('GITHUB_CLIENT_ID');
         $secret                     = env('GITHUB_SECRET');
@@ -92,8 +94,8 @@ class OAuthAction
         ];
 
         $client = $this->client->getClient([
-            'connect_timeout'   => 15,
-            'timeout'           => 15,
+            'connect_timeout'   => 10,
+            'timeout'           => 10,
             'headers'           => ['Accept' => 'application/json'],
         ]);
 
@@ -107,6 +109,39 @@ class OAuthAction
         $getAccessTokenJsonStr = $getAccessTokenResponse->getBody()->getContents();
         Log::info('获取 GitHub access_token 返回信息：' . $getAccessTokenJsonStr);
         $getAccessTokenArr = json_decode($getAccessTokenJsonStr, true);
+
+        if (!is_array($getAccessTokenArr) || !isset($getAccessTokenArr['access_token'])) {
+            return HttpUtil::error($response);
+        }
+
+        // 2、根据 GitHub 返回的 access_token 获取用户信息
+        $accessToken = $getAccessTokenArr['access_token'];
+        $getGitHubUserInfoUrl = 'https://api.github.com/user';
+        $client = $this->client->getClient([
+            'connect_timeout'   => 10,
+            'timeout'           => 10,
+            'headers'           => ['Accept' => 'application/json', 'Authorization' => 'token ' . $accessToken],
+        ]);
+
+        try {
+            $getGitHubUserInfoResponse = $client->request('GET', $getGitHubUserInfoUrl);
+        } catch (\Exception $exception) {
+            Log::error('获取 GitHub 用户信息异常！', ['code' => $exception->getCode(), 'msg' => $exception->getMessage()]);
+            return HttpUtil::error($response);
+        }
+
+        $getGitHubUserInfoStr = $getAccessTokenResponse->getBody()->getContents();
+        Log::info('获取 GitHub 用户信息返回信息：' . $getGitHubUserInfoStr);
+        $getGitHubUserInfoArr = json_decode($getGitHubUserInfoStr, true);
+
+        if (!is_array($getGitHubUserInfoArr) || !isset($getGitHubUserInfoArr['id'])) {
+            return HttpUtil::error($response);
+        }
+
+        $gitHubId = $getGitHubUserInfoArr['id'];
+        $gitHubName = $getGitHubUserInfoArr['login'];
+        $gitHubEmail = $getGitHubUserInfoArr['email'];
+        $gitHubAvatar = $getGitHubUserInfoArr['avatar_url'];
 
         return $response->redirect('/');
     }
