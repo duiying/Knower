f70d5e817b7c1d9b9c976d74505c97b964df015f
wangyaxian <wangyaxiandev@gmail.com>
2022-01-17 17:34:27 +0800

    init

--- storage/view/frontend/layouts/footer.blade.php
+++ storage/view/frontend/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="card-body" style="padding:15px;">
             Copyright © 2021
             <a target="_blank" href=""></a>&nbsp;|
-            <a target="_blank" href="http://www.miitbeian.gov.cn">备案号</a>&nbsp;|
+            <a target="_blank" href="https://beian.miit.gov.cn/">京ICP备2021040669号-1</a>&nbsp;|
             Powered by <a target="_blank" href="https://github.com/duiying/Knower">Knower</a>
         </div>
     </div>
