4683d6fb225d51d71ccdc8026798b62230251944
wangyaxian <wangyaxiandev@gmail.com>
2022-01-19 21:32:23 +0800

    init

--- storage/view/frontend/layouts/footer.blade.php
+++ storage/view/frontend/layouts/footer.blade.php
@@ -1,9 +1,10 @@
 <footer id="footer">
     <div class="card" style="margin-top:20px;text-align:center;">
         <div class="card-body" style="padding:15px;">
-            Copyright © 2021
-            <a target="_blank" href=""></a>&nbsp;|
-            <a target="_blank" href="https://beian.miit.gov.cn/">京ICP备2021040669号-1</a>&nbsp;|
+            <a target="_blank" href="https://beian.miit.gov.cn/">京ICP备2021040669号-1</a>
+            <br>
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11011102001817" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/storage/img/备案图标.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京公网安备 11011102001817号</p></a>
+            <br>
             Powered by <a target="_blank" href="https://github.com/duiying/Knower">Knower</a>
         </div>
     </div>
